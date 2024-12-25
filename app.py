29940659926c6bb487ade859a41cd0e1a24c2f49
amanattar <attar.aman29@gmail.com>
2024-12-25 11:11:57 -0600

    scraping

--- app.py
+++ app.py
@@ -15,15 +15,16 @@ from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 
 options = Options()
-options.binary_location = "/app/.apt/usr/bin/google-chrome"  # Path to Heroku's Chromium binary
+options.binary_location = "/app/.apt/opt/google/chrome/chrome"  # Path to Chrome binary
 options.add_argument("--disable-gpu")
-options.add_argument("--no-sandbox")  # Required for running as root in Heroku
-options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+# Specify the Chromedriver path
+service = Service("/app/.chromedriver/bin/chromedriver")
+driver = webdriver.Chrome(service=service, options=options)
 
 
 
